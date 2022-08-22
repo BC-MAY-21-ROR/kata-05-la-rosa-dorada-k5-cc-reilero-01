940e5c09d7cf0c32efcdbd074cfeb315052316f7
EduardoBriceno89 <eduardbrice@outlook.com>
2022-08-22 17:25:12 -0500

    Add first tests.
    co-authored-by: jorgeboris <eduardo.borisq@gmail.com>
    co-authored-by: GustavoHiguera <ghiguera@ucol.mx>

--- Gilded_Rose_test.rb
+++ Gilded_Rose_test.rb
@@ -1,10 +1,17 @@
-require File.join(File.dirname(__FILE__), 'gilded_rose')
+require File.join(File.dirname(__FILE__), 'Gilded_Rose')
 require 'test/unit'
 
-class TestUntitled < Test::Unit::TestCase
+class Gilded_rose_test < Test::Unit::TestCase
+
   def test_foo
     items = [Item.new('foo', 0, 0)]
     GildedRose.new(items).update_quality
-    assert_equal items[0].name, 'fixme'
+    assert_equal items[0].name, 'foo'
+  end
+
+  def special_item(item)
+    item = [Item.new("Sulfuras", 60, 80)]
+    Gilded_rose.new(item).update_quality
+    assert_equal item[0].quality, 80
   end
 end
